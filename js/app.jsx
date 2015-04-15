187f3b3018f60d9017372be80170b938bdab6f4b
Joe Innes <joe@joeinn.es>
2015-04-16 01:33:10 +0200

    Ready to begin working on API, styling, and segregation of views

--- js/app.jsx
+++ js/app.jsx
@@ -425,7 +425,7 @@ var SingleSessionBox = React.createClass({
           <th>End Time</th>
           <th>Duration</th>
         </tr>
-        <SingleSession session={this.props.session} />
+        <SingleSession session={this.props.session} crazymode />
       </table>
     )
   }
@@ -437,12 +437,38 @@ var SingleSession = React.createClass({
     // var d = new Date();
     var startTime = niceDate(session.startTime);
     var endTime = niceDate(session.endTime);
-    var sessions = this.props.session[0].trainees.map(function (trainee) {
-        return (
+    if ( this.props.crazymode ) {
+      var sessions = this.props.session[0].trainees.map(function (trainee) {
+          return (
+            <tr>
+              <td>{session.name}</td>
+              <td>{trainee}</td>
+              <td>{userObjFromName(trainee).empid}</td>
+              <td>{session.trainer}</td>
+              <td>{userObjFromName(session.trainer).empid}</td>
+              <td>{startTime.date}</td>
+              <td>{startTime.time}</td>
+              <td>{endTime.date}</td>
+              <td>{endTime.time}</td>
+              <td>{(session.endTime - session.startTime)/60} minutes</td>
+            </tr>
+          )
+      });
+      return (
+        <div>
+          {sessions}
+        </div>
+      )
+    } else {
+        var ids = [];
+          for ( var i = 0; i < session.trainees.length; i++ ) {
+            ids.push(userObjFromName(session.trainees[i]).empid);
+          }
+         return (
           <tr>
             <td>{session.name}</td>
-            <td>{trainee}</td>
-            <td>{userObjFromName(trainee).empid}</td>
+            <td><ItemList items={session.trainees} /></td>
+            <td><ItemList items={ids} /></td>
             <td>{session.trainer}</td>
             <td>{userObjFromName(session.trainer).empid}</td>
             <td>{startTime.date}</td>
@@ -452,13 +478,7 @@ var SingleSession = React.createClass({
             <td>{(session.endTime - session.startTime)/60} minutes</td>
           </tr>
         )
-    });
-    // console.log(sessions);
-    return (
-      <div>
-      {sessions}
-    </div>
-    )
+      }
   }
 });
 
